67748fb034f47af6ec4b4bcb5eefd754eb7fa5bf
Sunil Pandey <pandey.sunil1987@gmail.com>
2021-07-06 07:26:59 +0530

    Added infinite scroll

--- src/app/timeline/timeline-list.component.ts
+++ src/app/timeline/timeline-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, ElementRef } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { timelineInfo, ITimelineInfo } from "./timelineInfo";
 @Component({
   selector: 'app-timeline-list',
   template: `
     <div class="main-container">
       <app-timeline *ngFor="let item of list" [timelineInfo]="item"></app-timeline>
+      <div #listend style="height: 100px"></div>
     </div>
   `,
   styles: [
@@ -17,8 +18,19 @@ import { timelineInfo, ITimelineInfo } from "./timelineInfo";
     `,
   ],
 })
-export class TimelineListComponent {
+export class TimelineListComponent implements AfterViewInit {
   public list: ITimelineInfo[] = timelineInfo;
+  @ViewChild("listend") private listend!: ElementRef;
   constructor(private readonly hostElement: ElementRef<HTMLElement>) {
+  
   }
+  ngAfterViewInit(): void {
+    // Infinite scroll with intersection observer
+    const observer = new IntersectionObserver(([entry]) => {
+      this.list = [...this.list, ...timelineInfo];
+    }, {root: null});
+
+    observer.observe(this.listend.nativeElement)
+  }
+
 }
